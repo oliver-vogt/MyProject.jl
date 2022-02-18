0f1c08980a9f28f38b7694e226d8ea0aba44658d
Oliver Vogt <oliver.p.vogt@gmail.com>
2022-02-17 19:26:16 -0800

    changed tests

--- src/MyProject.jl
+++ src/MyProject.jl
@@ -1,5 +1,14 @@
 module MyProject
 
-# Write your package code here.
+using Expectations, Distributions
 
+function foo(μ = 1., σ = 2.)
+    println("Modified foo definition")
+    d = Normal(μ, σ)
+    E = expectation(d)
+    return E(x -> sin(x))
 end
+
+export foo
+
+end # module
